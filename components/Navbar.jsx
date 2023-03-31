f250c9be9d10401460384beb8737b057deb0cab9
amanansari247 <72137751+amanansari247@users.noreply.github.com>
2023-03-31 17:20:31 +0530

    Updated the image in Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Navbar() {
   return (
     <nav className='nav-head'>
-      <img src="./images/memegeneratorlogo.png" alt=""  className='nav-image'/>
+      <img src="https://i.ibb.co/1LKcCB6/memegeneratorlogo.png" className='nav-image'/>
       <h2 className='nav-title'>Meme Generator</h2>
       </nav>
   )
