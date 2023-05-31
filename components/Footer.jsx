41f1257d6e8ff0039ae79f521ae7e94395505884
amanansari247 <72137751+amanansari247@users.noreply.github.com>
2023-05-31 18:57:05 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className='footer'>
-      <p>Created with ❤ By Aman Hussain</p>
+      <p>Created with ❤ </p>
     </div>
   )
 }
